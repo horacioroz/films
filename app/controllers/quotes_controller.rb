0d921af0c35857f15b3ddfce6664e29fd70ea072
Horacio Rozanski <horacioroz@gmail.com>
2016-01-19 17:32:27 -0300

    second commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,9 +4,10 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM()").first
 	end
 	
-	def new
-		@quote = Quote.new
-	end
+	# def new
+	# 	@quote = Quote.new
+	# end 
+	#esto tendria que borrarse porque ya no se utiliza porque se utiliza el modal, lo dejo para pegar una repasada luego, lo mismo que new.html.erb
 	
 	def create
 		@quote = Quote.create(quote_params)
