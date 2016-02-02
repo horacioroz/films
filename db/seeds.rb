bd945055ba7d6a21fd0483b80fbd2d8618614792
Horacio Rozanski <horacioroz@gmail.com>
2016-02-02 12:17:28 -0300

    Update data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Quote.create({author: 'Pablo', saying: 'Stephen Curry es un animal'})
